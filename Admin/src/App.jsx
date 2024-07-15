c5a2d2ff5bc8f69979072d7ecb1084b95ca2eca0
ProgrammerKJ <121688585+ProgrammerKJ@users.noreply.github.com>
2024-07-15 17:14:28 -0500

    Update App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "https://food-delivery-be-as0u.onrender.com"";
+  const url = "https://food-delivery-be-as0u.onrender.com";
   return (
     <div>
       <ToastContainer />
